e7780a5d1489575b74e77b54cfc7207c06124db8
reena_savireddy <reena_savireddy@thbs.com>
2023-12-21 17:45:52 +0530

    comiting from master " ;

---
+++ frontend/frontend/ui/urls.py
@@ -0,0 +1,20 @@
+from django import views
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('',views.home,name="home"),
+    path('',views.home,name='home'),
+    path('login/',views.login,name='login'),
+    path('registration/',views.registration,name='registration'),
+    path('login/summarize',views.summarize,name='summarize'),
+    path('forgotpassword/',views.forgotpassword,name='forgotpassword'),
+    path('login/summarize/welcome',views.home,name='home'),
+    path('registration/login',views.login,name='login'),
+    path('registration/summarize',views.summarize,name='summarize'),
+    path('registration/welcome',views.home,name='home'),
+    path('login/welcome',views.home,name="home"),
+    path('login/registration',views.registration,name ="registration"),
+    path('login/forgotpassword',views.forgotpassword,name='forgotpassword'),
+    
+]
